c03fb5d6e1e08f459abd8dc4899556d201b2c458
voidxin <zhangxin409999@163.com>
2017-04-07 17:41:28 +0800

    log

---
+++ VXProtocolManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VXProtocolManager
+//
+//  Created by voidxin on 17/4/6.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VXProtocolManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VXProtocolManager
+//
+//  Created by voidxin on 17/4/6.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
